ef535620d9a36d36b41dea502dc41f495cdb82e8
DRR\user <daewon5656@naver.com>
2022-04-04 22:29:43 +0900

    이동원 변경

--- 통합(ver1.9)/브랜드 별 역사 및 특징/BH.js
+++ 통합(ver1.9)/브랜드 별 역사 및 특징/BH.js
@@ -1,39 +1,88 @@
 
 
-starbucks = document.getElementById("fullRapStarbucks")
-tomandtoms = document.getElementById(".fullRapTomandtoms *")
-ediya = document.getElementById(".fullRapEdiya *")
-hollys = document.getElementById(".fullRapHollys *")
+    
 
-
-starbucksEvery = starbucks.childNodes;
 
 
 function showContent(num){
 
-        console.log("잘넘어옴");
+      
     if(num == 1){
         
-        for(i=0; i < starbuck.length; i++){
-            starbucks[i].style.display = 'block'
-            tomandtoms[i].style.display = 'none'
-            ediya[i].style.display = 'none'
-            hollys[i].style.display = 'none'
+        for(i=0; i < document.querySelectorAll(".fullRapStarbucks *").length; i++){
+        document.querySelectorAll(".fullRapStarbucks *")[i].style.display = "inline-block";
         }
 
-        
+        for(i=0; i < document.querySelectorAll(".fullRapTomandtoms *").length; i++){
+            document.querySelectorAll(".fullRapTomandtoms *")[i].style.display = "none";
+            }
+
+            for(i=0; i < document.querySelectorAll(".fullRapEdiya *").length; i++){
+                document.querySelectorAll(".fullRapEdiya *")[i].style.display = "none";
+                }
+
+                for(i=0; i < document.querySelectorAll(".fullRapHollys *").length; i++){
+                    document.querySelectorAll(".fullRapHollys *")[i].style.display = "none";
+                    }
+
+
     } else if(num == 2){
-        document.getElementById("fullRapTomandtoms").style.display = "block";
-        console.log("잘넘어옴");
+         
+        for(i=0; i < document.querySelectorAll(".fullRapStarbucks *").length; i++){
+            document.querySelectorAll(".fullRapStarbucks *")[i].style.display = "none";
+            }
+    
+            for(i=0; i < document.querySelectorAll(".fullRapTomandtoms *").length; i++){
+                document.querySelectorAll(".fullRapTomandtoms *")[i].style.display = "inline-block";
+                }
+    
+                for(i=0; i < document.querySelectorAll(".fullRapEdiya *").length; i++){
+                    document.querySelectorAll(".fullRapEdiya *")[i].style.display = "none";
+                    }
+    
+                    for(i=0; i < document.querySelectorAll(".fullRapHollys *").length; i++){
+                        document.querySelectorAll(".fullRapHollys *")[i].style.display = "none";
+                        }
+    
     } else if(num == 3){
-        document.getElementById("fullRapEdiya").style.display = "block";
-        console.log("잘넘어옴");
-    } else if(num == 4){
-        document.getElementById("fullRapHollys").style.display = "block";
-        console.log("잘넘어옴");
-    }
+         
+        for(i=0; i < document.querySelectorAll(".fullRapStarbucks *").length; i++){
+            document.querySelectorAll(".fullRapStarbucks *")[i].style.display = "none";
+            }
+    
+            for(i=0; i < document.querySelectorAll(".fullRapTomandtoms *").length; i++){
+                document.querySelectorAll(".fullRapTomandtoms *")[i].style.display = "none";
+                }
+    
+                for(i=0; i < document.querySelectorAll(".fullRapEdiya *").length; i++){
+                    document.querySelectorAll(".fullRapEdiya *")[i].style.display = "inline-block";
+                    }
+    
+                    for(i=0; i < document.querySelectorAll(".fullRapHollys *").length; i++){
+                        document.querySelectorAll(".fullRapHollys *")[i].style.display = "none";
+                        }
+    
+    } else if(num == 4){    
+         
+        for(i=0; i < document.querySelectorAll(".fullRapStarbucks *").length; i++){
+            document.querySelectorAll(".fullRapStarbucks *")[i].style.display = "none";
+            }
+    
+            for(i=0; i < document.querySelectorAll(".fullRapTomandtoms *").length; i++){
+                document.querySelectorAll(".fullRapTomandtoms *")[i].style.display = "none";
+                }
+    
+                for(i=0; i < document.querySelectorAll(".fullRapEdiya *").length; i++){
+                    document.querySelectorAll(".fullRapEdiya *")[i].style.display = "none";
+                    }
+    
+                    for(i=0; i < document.querySelectorAll(".fullRapHollys *").length; i++){
+                        document.querySelectorAll(".fullRapHollys *")[i].style.display = "inline-block";
+                        }
     
 
+    }
+    
 
 
 
