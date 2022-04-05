e9885f99a72ad62c18910f2178d961ccd7c37016
DESKTOP-69SULOB\human <asd>
2022-04-05 12:38:32 +0900

    최종 주석 다는중 - 이동원

--- 통합(ver1.9)/슬라이드배너.js
+++ 통합(ver1.9)/슬라이드배너.js
@@ -1,3 +1,6 @@
+
+
+// 슬라이드의 인덱스 값, 제품 이미지들의 배열을 저장함
 var slideIndex = 0; //slide index
 var slideCoffeeIndex = 0; //slide index
       var arrImg1 = new Array("./이미지모음/커피 종류 사진/section2/스타벅스메뉴/돌체밀크티.jpg", "./이미지모음/커피 종류 사진/section2/스타벅스메뉴/딸기딜라이트요거트블렌디드.jpg",
@@ -58,24 +61,26 @@ var slideCoffeeIndex = 0; //slide index
 
 
 
-// HTML 로드가 끝난 후 동작
+// HTML 로드가 끝난 후 동작하는 함수
 window.onload=function (){
-  
+  // 첫 슬라이드를 보여준뒤
   showSlides(slideIndex);
 
-
-  // Auto Move Slide
-  var sec = 4000;
+  // 자동으로 돌아가는 시간 조절 4초
+      var sec = 4000;
   
-  setInterval(function(){
-    slideIndex++;
+  //슬라이드를 자동으로 돌아가도록 함수를 지정된 시간마다 작동시키는 setInterval함수
+    setInterval(function(){
+      slideIndex++;
    
-    showSlides(slideIndex);
-  }, sec);
+      showSlides(slideIndex);
+    }, sec);
 
 }
 
-// Next/previous controls
+
+
+// 이전, 다음 버튼을 통해 들어온 값으로 slideIndex의 값을 조절하여 슬라이드 조절
 function moveSlides(n) {
   slideIndex = slideIndex + n
   showSlides(slideIndex);
@@ -84,11 +89,11 @@ function moveSlides(n) {
 
 
 function showSlides(n) {
-
+  // 변수 선언
   var slides = document.getElementsByClassName("mySlides");
-  
   var size = slides.length;
 
+  // 슬라이드가 순서대로 다 돌았을 경우 다시 처음으로 가게 하는 if문
   if ((n+1) > size) {
     slideIndex = 0; n = 0;
   }else if (n < 0) {
@@ -96,9 +101,10 @@ function showSlides(n) {
     n = (size-1);
   }
 
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
+  
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
   
   slides[n].style.display = "block";
   
@@ -107,61 +113,9 @@ function showSlides(n) {
 
 
 
-
-// html 로드 되고 시작되는 두번째 함수
-
-
-
-// window.addEventListener('onload', function onclickSlide(num){
-
-
-
-//   imgArr = arrImg1;
-//   showSlides2(slideIndex2, imgArr);  
-
-
-  
-//     if(num == 1){
-//       imgArr = arrImg1;
-//     } else if(num == 2){
-//       imgArr = arrImg2;
-//     } else if (num == 3){
-//       imgArr = arrImg3;
-//     } else if(num == 4){
-//       imgArr = arrImg4;
-//     } 
-
-//   // Auto Move Slide
-  
-//   var sec2 = 5000;
-
-//   setInterval(function(){
-    
-//     slideIndex2++;
-//      showSlides2(slideIndex2, imgArr);
-
-//   }, sec2);
-
-// });
-
-
-// function onclickSlide(ele){
-
-//   if(ele.value == 1){
-//       return arrImg1;
-//   } else if(ele.value == 2){
-//     return arrImg2;
-      
-//   } else if (ele.value == 3){
-//     return arrImg3;
-//   } else if(ele.value == 4){
-//     return arrImg4;
-//   } 
-
-// }
-
-
+// 브랜드 버튼을 누르면 해당 브랜드의 제품을 보여주는 함수
 function showSlidesCoffee(num) {
+  // num을 통해 어떤 브랜드 인지 판별하는 if문
   if(num == 1){
     imgArr = arrImg1;
   } else if(num == 2){
@@ -172,72 +126,39 @@ function showSlidesCoffee(num) {
     imgArr = arrImg4;
   } 
  
+  // if문을 통해 판별된 브랜드를 통해 
+  // 해당 브랜드 배열의 첫번째 이미지를 나타냄
   document.getElementById("slideImg").src = imgArr[slideCoffeeIndex];
 }
 
 
-  function printCoffee(n){
-
-    var size = imgArr.length;
-
-  if ((n+1) > size) {
-    slideCoffeeIndex = 0; n = 0;
-  }else if (n < 0) {
-    slideCoffeeIndex = (size-1);
-    n = (size-1);
-  }
-  
- 
-  document.getElementById("slideImg").src = imgArr[slideCoffeeIndex];
-  }
 
 
-
-// Next/previous controls
+// 이전, 다음을 통해 slideCoffeIndex의 값을 -1 또는 +1로 배열의 인덱스를 조절
+// 그리고 다음 함수 printCoffee 실행
 function moveSlidesCoffee(n) {
   slideCoffeeIndex = slideCoffeeIndex + n
   printCoffee(slideCoffeeIndex);
 }
 
 
+
+// 첫 함수가 배열의 첫번째 이미지를 보여준다면
+// printCoffee는 다음 이미지의 회전을 나타내는 함수
+
+function printCoffee(n){
+
+  var size = imgArr.length;
+
+      if ((n+1) > size) {
+        slideCoffeeIndex = 0; n = 0;
+      }else if (n < 0) {
+        slideCoffeeIndex = (size-1);
+        n = (size-1);
+      }
+
+document.getElementById("slideImg").src = imgArr[slideCoffeeIndex];
+}
+
+
   
-
-
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-
-
-
-// function showSlides2(n) {
-  
-//   let arrImg = new Array("./이미지모음/커피 종류 사진/스타벅스/스벅 아아.jpg", 
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 뜨아.jpg", "./이미지모음/커피 종류 사진/스타벅스/스벅 돌체 밀크티.jpg",
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 딸기 딜라이트 요거트 블렌디드.jpg","./이미지모음/커피 종류 사진/스타벅스/스벅 바닐라크림 프라푸치노.jpg",
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 아이스 돌체 블랙 밀크티.jpg", "./이미지모음/커피 종류 사진/스타벅스/스벅 아이스 라떼.jpg", 
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 아이스 카라멜 마키야또.jpg","./이미지모음/커피 종류 사진/스타벅스/스벅 아이스 카푸치노.jpg",
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 아이스 캐모마일 블랜드 티.jpg","./이미지모음/커피 종류 사진/스타벅스/스벅 아이스 히비스커스 블렌드 티.jpg",
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 자바칩 프라푸치노.jpg","./이미지모음/커피 종류 사진/스타벅스/스벅 에스프레소 프라푸치노.jpg",
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 제주 그린 한라봉 모히또 블렌디드.jpg", "./이미지모음/커피 종류 사진/스타벅스/스벅 카라멜 마키야또.jpg",
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 카페라떼.jpg","./이미지모음/커피 종류 사진/스타벅스/스벅 카푸치노.jpg",
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 캐모마일 블렌드 티.jpg", "./이미지모음/커피 종류 사진/스타벅스/스벅 콜드브루.jpg",
-//   "./이미지모음/커피 종류 사진/스타벅스/스벅 트위스트 피치 요거트 블렌디드.jpg","./이미지모음/커피 종류 사진/스타벅스/스벅 히비스커스 블렌드 티.jpg");
-  
-//   document.getElementById("slideImg").src = arrImg;
-
-//   var slides2 = document.getElementById("slideImg")
-//   var size2 = slides2.length;
-
-
-
-//   if ((n+1) > size2) {
-//     slideIndex2 = 0; n = 0;
-//   }else if (n < 0) {
-//     slideIndex2 = (size2-1);
-//     n = (size2-1);
-//   }
-
-//   for (i = 0; i < slides2.length; i++) {
-//       slides2[i].style.display = "none";
-//   }
-
-//   slides2[n].style.display = "block";
-// }
