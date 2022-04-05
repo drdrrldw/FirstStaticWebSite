524e08819eec95a73ad547d0f8a789b6cf002cf8
DESKTOP-69SULOB\human <asd>
2022-04-05 17:33:41 +0900

    0405 수업 끝나기전 마지막 커밋

---
+++ 0405수업/DOM연습.js
@@ -0,0 +1,82 @@
+
+
+
+function changeYellow(){
+    let el = document.getElementById("loremTitle1");
+    // 한번 더 클릭하면 다시 검은색으로 바꿔보세요. 왔다리갔다리
+    if(el.style.color == 'yellow'){
+        el.style.color = 'black';
+    }else{
+        el.style.color = 'yellow';
+    }
+}
+
+//빨간색 배경으로 왔다리 갔다리
+function toggleRed(){
+    let el = document.getElementsByClassName("changeRed");
+    for (let i of el){
+        if(i.style.backgroundColor == 'red'){
+            i.style.backgroundColor = null;
+        }else{
+            i.style.backgroundColor = 'red';
+        }
+    }
+}
+
+function useCssSelector(){
+    let el = document.querySelector('#para1');
+    el.style.color = "green";
+}
+
+
+
+function makeDom(){
+    let heading = document.createElement("h1");
+    let maked_text = document.createTextNode("동적으로로 만든 컨텐츠");
+    heading.appendChild(maked_text);
+    console.log(heading);
+    document.getElementById("here").appendChild(heading);
+}
+
+
+
+
+function chSizeDouble(){
+    let image = document.getElementsByTagName('img');
+    for(let el of image){
+        el.width *= 2;
+    }
+}
+
+function chSizeHalf(){
+    let image = document.getElementsByTagName('img');
+    for(let el of image){
+        el.width *= 0.5;
+    }
+}
+
+
+// 1. 순수 자바스크립트
+let a = document.querySelector('#where1');
+a.addEventListener('click', whereTouch1);
+
+// 2. 제이쿼리 사용
+let b = $('#where2')
+b.click(whereTouch2);
+
+
+
+function whereTouch1(){
+    
+    alert("어딜만져요오옷!");
+}
+
+function whereTouch2(){
+    alert("어딜만져요오올읾ㄴ이라ㅓ!");
+}
+
+
+
+
+토글
+1. 순수 자바스크립트
